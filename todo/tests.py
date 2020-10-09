47a4e41742ee6945554cccd934ba0648697a3722
duyhuynh02 <minhduyhuynh16@gmail.com>
2020-10-09 11:20:52 +0700

    update indentation

--- todo/tests.py
+++ todo/tests.py
@@ -9,14 +9,11 @@ import datetime
 class TodoTests(TestCase):
 
     def setUp(self):
-
         self.user = get_user_model().objects.create_user(
             username='testuser',
             email='test@email.com',
             password='secret',
         )
-
-
         self.post = Post.objects.create(
             title='A test title',
             memo='A test content',
@@ -24,11 +21,9 @@ class TodoTests(TestCase):
             completed=False,
         )
 
-
     def test_string_represent(self):
         post = Post(title='A sample title')
         self.assertEqual(str(post), post.title)
-
 
     def test_post_component(self):
         self.assertEqual(f'{self.post.title}', 'A test title')
@@ -36,13 +31,11 @@ class TodoTests(TestCase):
         self.assertEqual(f'{self.post.author}', 'testuser')
         self.assertEqual(f'{self.post.completed}', 'False')
 
-
     def test_post_list_view(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'A test content')
         self.assertTemplateUsed(response, 'home.html')
-
 
     def test_post_detail_view(self):
         response = self.client.get('/work/1')
--- users/tests.py
+++ users/tests.py
@@ -3,28 +3,23 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse 
 
 
-
 class SignUpPageTests(TestCase):
 
     username = 'newuser'
     email = 'newuser@gmail.com'
 
-
     def test_signup_page_status_code(self):
         response = self.client.get('/users/signup/')
         self.assertEqual(response.status_code, 200)
-
 
     def test_view_url_by_name(self):
         response = self.client.get(reverse('signup'))
         self.assertEqual(response.status_code, 200)
 
-
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('signup'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'signup.html')
-
 
     def test_signup_form(self):
         new_user = get_user_model().objects.create_user(
